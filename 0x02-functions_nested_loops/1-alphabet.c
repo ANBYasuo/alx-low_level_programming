0a0b388f0e59b7c96e367d7f1fd4eac878ba795f
Mohamed Hosam <mido.hosamcr8@gmail.com>
2023-03-21 08:26:06 -0700

    committing

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - check the code
+* Return: 0 (Success)
+*/
+int void print_alphabet(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+return (0);
+}
