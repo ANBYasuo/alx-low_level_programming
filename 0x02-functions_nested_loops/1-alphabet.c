0695ed8f9421acb7ad7e96a2037313ceae314934
Mohamed Hosam <mido.hosamcr8@gmail.com>
2023-03-21 08:31:15 -0700

    committing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
 * main - check the code
 * Return: 0 (Success)
 */
-int void print_alphabet(void);
+void print_alphabet(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
 		_putchar(x);
 	_putchar('\n');
-return (0);
 }
